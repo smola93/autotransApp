d8d77408aed003b9e77eef585605649c81c59291
smola93 <mateuszsamulnik@gmail.com>
2020-10-09 22:34:02 +0200

    inputs done

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ mainOptions.addEventListener('change', function () {
 
   const mech = document.querySelector('.mech');
   const paint = document.querySelector('.paint');
+  const other = document.querySelector('.other');
 
   if (mainOptions.value == "mech") {
     mech.style.display = "flex";
     paint.style.display = "none";
+    other.style.display = "none";
     console.log("Wybrano opcje naprawy mechanicznej");
   } else if (mainOptions.value == "paint") {
     mech.style.display = "none";
     paint.style.display = "flex";
+    other.style.display = "none";
     console.log("Wybrano opcje naprawy blacharsko-lakierniczej");
-  } else {
+  } else if (mainOptions.value == "other") {
     mech.style.display = "none";
     paint.style.display = "none";
+    other.style.display = "flex";
+    console.log("Wybrano opcje innej naprawy");
   }
 });
